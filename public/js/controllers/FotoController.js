b45e98a4228d7ca5cd5d0867640f7bd0eb070817
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2016-08-18 17:30:21 -0300

    cap. 7 - post, mensagem de sucesso/erro

--- public/js/controllers/FotoController.js
+++ public/js/controllers/FotoController.js
@@ -1,6 +1,14 @@
 angular.module('alurapic').controller('FotoController', function($scope, $http){
     $scope.salvar = function(){
-        var foto = $scope.foto;
-        //$http.post();
+        if($scope.formulario.$valid){
+            var foto = $scope.foto;
+            $http.post('/v1/fotos', foto)
+            .success(function(){
+                $scope.mensagem = 'Foto cadastrada com sucesso!';
+            })
+            .error(function(erro){
+                $scope.mensagem = 'Não  possível cadastrar a foto: ' + erro;
+            });
+        }
     }
 });
