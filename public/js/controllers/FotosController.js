abe4bc669e46e91ae3d633cef32cf983c6030125
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2016-08-18 13:19:22 -0300

    cap. 3 - $http

--- public/js/controllers/FotosController.js
+++ public/js/controllers/FotosController.js
@@ -1,8 +1,10 @@
 angular.module('alurapic')
-.controller('FotosController', function ($scope) {
-    var fotos = [];
-    fotos.push({ url: "http://www.fundosanimais.com/Minis/leoes.jpg", titulo: "Leão"});
-    fotos.push({ url: "http://www.fundosanimais.com/Minis/cao.jpg", titulo: "Cão"});
-    fotos.push({ url: "http://www.fundosanimais.com/Minis/gato-branco.jpg", titulo: "Gato Branco"});
-    $scope.fotos = fotos;
+.controller('FotosController', function ($scope, $http) {
+    $http.get('v1/fotos')
+    .success(function(fotos){
+        $scope.fotos = fotos;
+    })
+    .error(function(erro){
+        console.log(erro);
+    });
 });
