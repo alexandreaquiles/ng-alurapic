cf5c55c2bd88c932fc674dc7948e1664f24a4cfb
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2016-08-20 09:48:45 -0300

    cap. 10 - $resource em FotosController (query e remove)

--- public/js/controllers/FotosController.js
+++ public/js/controllers/FotosController.js
@@ -1,21 +1,18 @@
 angular.module('alurapic')
-.controller('FotosController', function ($scope, $http) {
-    $http.get('v1/fotos')
-    .success(function(fotos){
+.controller('FotosController', function ($scope, $resource) {
+    var Fotos = $resource('v1/fotos/:fotoId');
+    Fotos.query(function(fotos){
         $scope.fotos = fotos;
-    })
-    .error(function(erro){
+    }, function(erro){
         console.log(erro);
     });
 
     $scope.remover = function(foto){
-        $http.delete('/v1/fotos/' + foto._id)
-        .success(function() {
+        Fotos.remove({fotoId: foto._id}, function() {
              var indiceDaFoto = $scope.fotos.indexOf(foto);
              $scope.fotos.splice(indiceDaFoto, 1);
              $scope.mensagem =  ('Foto ' + foto.titulo + ' removida com sucesso!');
-        })
-        .error(function(erro) {
+        }, function(erro) {
              $scope.mensagem = ('Não foi possível apagar a foto ' + foto.titulo);
         });
     };
